cd0ed3f57bdb1d8dbd2a4606c049fa7827b63f72
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-04-03 13:54:12 +0200

    Mvvm helpers.

---
+++ Windows8vb/NetEduApp/Mvvm/ViewModelBase.vb
@@ -0,0 +1,11 @@
+﻿Namespace Mvvm
+	Public MustInherit Class ViewModelBase
+		Implements INotifyPropertyChanged
+
+		Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+		Protected Sub RaisePropertyChanged(propertyName As String)
+			RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
+		End Sub
+	End Class
+End Namespace
