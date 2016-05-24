e97852d303b707e08cda9fe53edcb778167e8960
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-05-25 00:32:19 +0200

    [Emulator] Internals will be visible for Test project.

--- Shared/NetEduApp.Emulators/Properties/AssemblyInfo.cs
+++ Shared/NetEduApp.Emulators/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NetEduApp.Emulators")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Maciej Klemarczyk 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -28,3 +28,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("NetEduApp.Emulators.Tests")]
+[assembly: InternalsVisibleTo("NetEduApp.Emulators.Fakes")]
