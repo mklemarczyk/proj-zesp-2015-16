431ac379707e13da283aaca2155cf5451785fc34
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-05-26 17:44:57 +0200

    [Emulator] Emulator logger were improved to handle more information and moved to separate namespace.

---
+++ Shared/NetEduApp.Emulators/Logger/EventType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetEduApp.Emulators.Logger {
+    public enum EventType {
+        #region Connection
+        Connected,
+        Disconnected,
+        NotConnected,
+        #endregion
+
+        #region Packet transmission
+        PacketSend,
+        PacketRecived,
+        #endregion
+    }
+}
