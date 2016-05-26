b267b42d77806981bdff353b3487820ec6e84f2d
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-05-27 00:59:14 +0200

    [Emulator] New logger event types were created. Diagnostic print was implemented in debug mode.

--- Shared/NetEduApp.Emulators/Logger/EventType.cs
+++ Shared/NetEduApp.Emulators/Logger/EventType.cs
@@ -5,16 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace NetEduApp.Emulators.Logger {
-    public enum EventType {
-        #region Connection
-        Connected,
-        Disconnected,
-        NotConnected,
-        #endregion
+	public enum EventType {
+		#region Connection
+		Connected,
+		Disconnected,
+		NotConnected,
+		#endregion
 
-        #region Packet transmission
-        PacketSend,
-        PacketRecived,
-        #endregion
-    }
+		#region Routing
+		RouteFoundConnected,
+		RouteFound,
+		RouteDefaultUsed,
+		RouteNotFound,
+		#endregion
+
+		#region Packet transmission
+		PacketSend,
+		PacketRecived,
+		PacketRouted,
+		HubPacketColision,
+		#endregion
+	}
 }
