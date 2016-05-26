8676fb2c2f4dcdb5354e9c959cc2b2ca08d853a3
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-05-27 00:52:32 +0200

    [Emulator] Interface connection validation was implemented. Additional interface name validation was implemented. NetHwInterface disconnect method was fixed.

--- Shared/NetEduApp.Emulators/Network/NetHwInterface.cs
+++ Shared/NetEduApp.Emulators/Network/NetHwInterface.cs
@@ -7,63 +7,71 @@ using NetEduApp.Emulators.Logger;
 using NetEduApp.Emulators.Network.Abstract;
 
 namespace NetEduApp.Emulators.Network {
-    internal class NetHwInterface : INetHwInterface {
-        private INetDevice parent;
-        private INetHwInterface otherInterface;
-        private string name;
+	internal class NetHwInterface : INetHwInterface {
+		private INetDevice parent;
+		private INetHwInterface otherInterface;
+		private string name;
 
-        internal NetHwInterface(INetDevice parent, string name) {
-            if (parent == null)
-                throw new ArgumentNullException("parent");
-            if (name == null)
-                throw new ArgumentNullException("name");
-            this.parent = parent;
-            this.name = name;
-        }
+		internal NetHwInterface(INetDevice parent, string name) {
+			if (parent == null)
+				throw new ArgumentNullException("parent");
+			if (name == null)
+				throw new ArgumentNullException("name");
+			if(name == string.Empty)
+				throw new ArgumentException("name can not be empty");
+			this.parent = parent;
+			this.name = name;
+		}
 
-        public INetDevice Parent { get { return this.parent; } }
-        public string Name { get { return this.name; } }
+		public INetDevice Parent { get { return this.parent; } }
+		public string Name { get { return this.name; } }
 
-        public virtual void Connect(INetHwInterface other) {
-            if(other is NetHwInterface) {
-                this.Connect((NetHwInterface)other);
-            } else {
-                throw new ArgumentException("Invalid other type, it require [NetHwInterface] object");
-            }
-        }
+		public virtual void Connect(INetHwInterface other) {
+			if (other == null)
+				throw new ArgumentNullException("other");
+			if (ReferenceEquals(this, other))
+				throw new ArgumentException("Can not connect the same interface instances");
+			if(ReferenceEquals(this.otherInterface, other))
+				throw new ArgumentException("That connection was made already");
+			if (other is NetHwInterface) {
+				this.Connect((NetHwInterface)other);
+			} else {
+				throw new ArgumentException("Invalid other type, it require [NetHwInterface] object");
+			}
+		}
 
-        public virtual void Disconnect() {
-            var other = otherInterface;
-            otherInterface.Disconnect( );
-            if (other != null) {
-                other.Disconnect( );
-            }
-            EmulatorLogger.Log(LogLevel.Info, EventType.Disconnected, this.Name);
-        }
+		public virtual void Disconnect( ) {
+			var other = otherInterface;
+			if (other != null) {
+				otherInterface = null;
+				EmulatorLogger.Log(LogLevel.Info, EventType.Disconnected, this.Name);
+				other.Disconnect( );
+			}
+		}
 
-        private void Connect(NetHwInterface other) {
-            if (otherInterface != null) {
-                otherInterface.Disconnect( );
-            }
-            if (other != null) {
-                other.Disconnect( );
-                other.otherInterface = this;
-            }
-            otherInterface = other;
-            EmulatorLogger.Log(LogLevel.Info, EventType.Connected, this.Name);
-        }
+		private void Connect(NetHwInterface other) {
+			if (otherInterface != null) {
+				otherInterface.Disconnect( );
+			}
+			if (other != null) {
+				other.Disconnect( );
+				other.otherInterface = this;
+			}
+			otherInterface = other;
+			EmulatorLogger.Log(LogLevel.Info, EventType.Connected, this.Name);
+		}
 
-        public virtual void ReceiveData(INetPacket data) {
-            EmulatorLogger.Log(LogLevel.Info, EventType.PacketRecived, this.Name);
-            if (data == null)
-                throw new ArgumentNullException("data");
-            if (data.DestinationInterface == this)
-                parent.ReceiveData(data);
-        }
+		public virtual void ReceiveData(INetPacket data) {
+			EmulatorLogger.Log(LogLevel.Info, EventType.PacketRecived, this.Name);
+			if (data == null)
+				throw new ArgumentNullException("data");
+			if (data.DestinationInterface == this)
+				parent.ReceiveData(data, this);
+		}
 
-        public virtual void SendData(INetPacket data) {
-            EmulatorLogger.Log(LogLevel.Info, EventType.PacketSend, this.Name);
-            otherInterface.ReceiveData(new NetPacket(this, otherInterface, data.SourceAddress, data.DestinationAddress));
-        }
-    }
+		public virtual void SendData(INetPacket data) {
+			EmulatorLogger.Log(LogLevel.Info, EventType.PacketSend, this.Name);
+			otherInterface.ReceiveData(new NetPacket(this, otherInterface, data.SourceAddress, data.DestinationAddress));
+		}
+	}
 }
