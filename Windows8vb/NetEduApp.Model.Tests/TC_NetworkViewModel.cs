a9a42b3d7e4d56d032c50ccc72016e9c941a1227
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-06-05 20:30:43 +0200

    [Win8App][Tests] Test project was created.

---
+++ Windows8vb/NetEduApp.Model.Tests/TC_NetworkViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace NetEduApp.Model.Tests
+{
+    [TestClass]
+    public class TC_NetworkViewModel
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
