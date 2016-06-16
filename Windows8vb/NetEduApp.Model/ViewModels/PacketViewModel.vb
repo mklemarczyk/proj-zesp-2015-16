123c9a95be64003b90f94bf4c2075cc75abba6f4
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2016-06-16 12:28:53 +0200

    PacketViewModel was impelemted to use.

--- Windows8vb/NetEduApp.Model/ViewModels/PacketViewModel.vb
+++ Windows8vb/NetEduApp.Model/ViewModels/PacketViewModel.vb
@@ -1,8 +1,43 @@
 ﻿Imports NetEduApp.Model.Common
+Imports NetEduApp.Simulator
 
 Namespace ViewModels
-	Public Class PacketViewModel
-		Inherits ViewModelBase
+    Public Class PacketViewModel
+        Inherits ViewModelBase
 
-	End Class
+        Private _SourceAddress As NetIpAddress
+        Private _TargetAddress As NetIpAddress
+        Private _Protocol As String
+
+        Public Property SourceAddress As NetIpAddress
+            Get
+                Return _SourceAddress
+            End Get
+            Set(value As NetIpAddress)
+                _SourceAddress = value
+                RaisePropertyChanged()
+            End Set
+        End Property
+
+        Public Property TargetAddress As NetIpAddress
+            Get
+                Return _TargetAddress
+            End Get
+            Set(value As NetIpAddress)
+                _TargetAddress = value
+                RaisePropertyChanged()
+            End Set
+        End Property
+
+        Public Property Protocol As String
+            Get
+                Return _Protocol
+            End Get
+            Set(value As String)
+                _Protocol = value
+                RaisePropertyChanged()
+            End Set
+        End Property
+
+    End Class
 End Namespace
